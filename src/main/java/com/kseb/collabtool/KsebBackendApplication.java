2ce8257d8522b1ef5eb8097e853bbf280371879e
JaeHyunAn <98042706+yyuneu@users.noreply.github.com>
2025-07-03 02:23:09 +0900

    최초 커밋

---
+++ src/main/java/com/kseb/collabtool/KsebBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kseb.collabtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsebBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KsebBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kseb/collabtool/KsebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kseb.collabtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsebBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
