5925f4d9d369ce453678fb88813290ecf819c6b8
TaeYeon Hwang <ty5321@naver.com>
2025-07-12 20:19:29 +0900

    feat: EventTask 조회 기능

--- src/main/java/com/kseb/collabtool/domain/events/repository/EventTaskRepository.java
+++ src/main/java/com/kseb/collabtool/domain/events/repository/EventTaskRepository.java
@@ -4,5 +4,10 @@ package com.kseb.collabtool.domain.events.repository;
 import com.kseb.collabtool.domain.events.entity.EventTask;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EventTaskRepository extends JpaRepository<EventTask, Long> {
+    List<EventTask> findByEvent_Id(Long eventId);
+    // 한 유저가 assignee인 모든 Task 조회
+    List<EventTask> findByAssignee_Id(Long assigneeId);
 }
