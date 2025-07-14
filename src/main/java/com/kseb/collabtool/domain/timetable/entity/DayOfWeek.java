e679e5ba0bfec76d07bfd852430e43ac6379c9b7
chan1606 <leechan1606@kyonggi.ac.kr>
2025-07-14 15:00:16 +0900

    cource 시간표 기능 추가

--- src/main/java/com/kseb/collabtool/domain/timetable/entity/DayOfWeek.java
+++ src/main/java/com/kseb/collabtool/domain/timetable/entity/DayOfWeek.java
@@ -1,5 +1,8 @@
 package com.kseb.collabtool.domain.timetable.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.STRING)
 public enum DayOfWeek {
     MO, TU, WE, TH, FR, SA, SU;
 }
