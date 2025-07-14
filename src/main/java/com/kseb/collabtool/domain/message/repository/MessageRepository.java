acc75773f449df5044c4965a8443d45d35908660
SaeByeok22 <icon0615@kyonggi.ac.kr>
2025-07-14 17:07:22 +0900

    update message exceptionHandler

--- src/main/java/com/kseb/collabtool/domain/message/repository/MessageRepository.java
+++ src/main/java/com/kseb/collabtool/domain/message/repository/MessageRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    // 생성일 기준으로 삭제되지 않은 메시지를 정렬해서 반환
     List<Message> findByChannelIdAndDeletedFalseOrderByCreatedAtAsc(Long channelId);
+
 }
--- src/main/java/com/kseb/collabtool/domain/message/repository/MessageTypeRepository.java
+++ src/main/java/com/kseb/collabtool/domain/message/repository/MessageTypeRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MessageTypeRepository extends JpaRepository<MessageType, Short> {
+    // 메시지 타입에 따른 엔티티 조회
     Optional<MessageType> findByCode(String code);
 }
